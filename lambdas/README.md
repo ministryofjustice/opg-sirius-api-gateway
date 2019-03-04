39ca52fb39c7a27b0afe874fbeda80520b91b0a3
Neil Smith <github@nsmith.me.uk>
2019-03-04 17:19:31 +0000

    Added tests for data_providers/JsonProvider

--- lambdas/README.md
+++ lambdas/README.md
@@ -7,23 +7,26 @@ Each function should have its own Python file in the root of this directory.
 * Python 3.7
 * pip3
 
-### Installing dependencies
+### Packaging for deployment to Lambda
 
 From within `opg-sirius-api-gateway/lambdas`
 
 ```bash
 pip3 install -r requirements.txt  --target ./vendor
+zip -r9 ../lambda.zip .
 ```
 
 This will install the requirements defined in requirements.txt, into `./lib`
 
-### Compressing files for upload to AWS
+And then it will create `lambda.zip` in the root of the repository. All required modules installed by pip will be included
+within the zip file.
+
+### Local development
 
 From within `opg-sirius-api-gateway/lambdas`
 
 ```bash
-zip -r9 ../lambda.zip .
+python3 -m virtualenv .venv
+source .venv/bin/activate
+pip install -r requirements.txt -r requirements-dev.txt
 ```
-
-This will create `lambda.zip` in the root of the repository. All required modules installed by pip will be included
-within the zip file.
--- lambdas/data_providers/json_provider.py
+++ lambdas/data_providers/json_provider.py
@@ -1,6 +1,5 @@
-import os
 import json
-from datetime import datetime
+from datetime import datetime, timezone
 
 # --------------------------------------------
 # Responsible for looking up a given LPA from
@@ -8,25 +7,32 @@ from datetime import datetime
 
 
 class JsonProvider:
-    def __init__(self):
-        path = os.path.join(os.path.dirname(__file__), 'example.json')  # File is relative to this script
-        with open(path) as f:
-            self.json_data = json.load(f)
-            print("Loaded JSON testing data")
+    def __init__(self, data_path):
+        self._lpas = None
+        self._data_path = data_path
+
+    def _get_data(self):
+        if not isinstance(self._lpas, dict):
+            # path = os.path.join(os.path.dirname(__file__), 'example.json')  # File is relative to this script
+            with open(self._data_path) as f:
+                self._lpas = json.load(f)
+                print("Loaded JSON testing data")
+
+        return self._lpas
 
     def get_lpa_by_sirius_uid(self, sirius_uid):
-        for lpa in self.json_data:
+        for lpa in self._get_data():
             if 'uid' in lpa and lpa['uid'] == sirius_uid:
                 return {
-                    'metadata': {'date': datetime.utcnow().isoformat()+'Z'},
+                    'metadata': {'date': datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()},
                     'payload': lpa
                 }
 
     def get_lpa_by_lpa_online_tool_id(self, online_tool_id):
-        for lpa in self.json_data:
+        for lpa in self._get_data():
             if 'onlineLpaId' in lpa and lpa['onlineLpaId'] == online_tool_id:
                 return {
-                    'metadata': {'date': datetime.utcnow().isoformat()+'Z'},
+                    'metadata': {'date': datetime.utcnow().replace(tzinfo=timezone.utc).isoformat()},
                     'payload': lpa
                 }
 
