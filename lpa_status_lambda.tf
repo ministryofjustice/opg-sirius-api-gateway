b0d1af60f7218a1dd10c89f588007b397be68f93
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-03-06 16:27:24 +0000

    Create the lpa status lambda

---
+++ lpa_status_lambda.tf
@@ -0,0 +1,47 @@
+data "aws_vpc" "vpc" {
+  filter {
+    name   = "tag:Stack"
+    values = ["${local.vpc_name}"]
+  }
+}
+
+resource "aws_security_group" "lpa_status" {
+  name        = "lpa_status"
+  description = "LPA Status Security Group"
+  vpc_id      = "${data.aws_vpc.vpc.id}"
+}
+
+module "lpa_status" {
+  source = "modules/api_gateway_lambda_function"
+
+  lambda_name              = "lpa_status"
+  lambda_function_filename = "${path.module}/lambdas/lpa_status_lambda.zip"
+  lambda_runtime           = "python3.7"
+
+  security_group_ids = [
+    "${aws_security_group.lpa_status.id}",
+  ]
+
+  vpc                          = "${local.vpc_name}"
+  account_id                   = "${local.target_account}"
+  api_gateway                  = "${aws_api_gateway_rest_api.opg_api_gateway.name}"
+  api_gateway_deployment_stage = "testing-0-0-1"
+
+  permitted_consumer_roles = [
+    "arn:aws:iam::${lookup(local.accounts, "sandbox")}:root",
+    "arn:aws:iam::${lookup(local.accounts, "sandbox")}:role/SandboxPoweruser",
+    "arn:aws:iam::${lookup(local.accounts, "lpa-development")}:role/api2.*",
+  ]
+
+  environment {
+    variables {
+      USER = "user-one"
+    }
+  }
+
+  tags = "${local.default_tags}"
+}
+
+output "lpa_status_invoke_url" {
+  value = "${module.lpa_status.lambda_name} invoke URL: ${module.lpa_status.lambda_invoke_url}"
+}
