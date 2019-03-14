7460bf6bafaf1502e223c8be34aec0fbd420b609
Andrew Pearce <andrew.pearce@digital.justice.gov.uk>
2019-03-14 09:25:06 +0000

    WIP Use existing certificate in sirius dev

--- api-gateway.tf
+++ api-gateway.tf
@@ -35,50 +35,33 @@ resource "aws_iam_role_policy_attachment" "log_to_cloudwatch" {
 
 resource "aws_api_gateway_domain_name" "opg_api_gateway" {
   domain_name              = "api.sirius.opg.digital"
-  regional_certificate_arn = "${aws_acm_certificate_validation.opg_api_gateway.certificate_arn}"
+  regional_certificate_arn = "${data.aws_acm_certificate.dev_sirius_opg_digital.arn}"
 
   endpoint_configuration {
     types = ["REGIONAL"]
   }
 }
 
-# Domain names and Certificates are provisioned in the Management account
-data "aws_route53_zone" "sirius_opg_digital" {
-  name = "sirius.opg.digital."
+data "aws_route53_zone" "dev_sirius_opg_digital" {
+  name = "dev.sirius.opg.digital."
+}
+
+data "aws_acm_certificate" "dev_sirius_opg_digital" {
+  domain      = "*.dev.sirius.opg.digital"
+  types       = ["AMAZON_ISSUED"]
+  most_recent = true
 }
 
 resource "aws_route53_record" "opg_api_gateway" {
   name    = "api.sirius.opg.digital"
   type    = "A"
-  zone_id = "${data.aws_route53_zone.sirius_opg_digital.id}"
+  zone_id = "${data.aws_route53_zone.dev_sirius_opg_digital.id}"
 
   alias {
     evaluate_target_health = true
     name                   = "${aws_api_gateway_domain_name.opg_api_gateway.regional_domain_name}"
     zone_id                = "${aws_api_gateway_domain_name.opg_api_gateway.regional_zone_id}"
   }
-}
-
-resource "aws_acm_certificate" "opg_api_gateway" {
-  domain_name       = "api.sirius.opg.digital"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "aws_route53_record" "opg_api_gateway_certificate_validation" {
-  name    = "${aws_acm_certificate.opg_api_gateway.domain_validation_options.0.resource_record_name}"
-  type    = "${aws_acm_certificate.opg_api_gateway.domain_validation_options.0.resource_record_type}"
-  zone_id = "${data.aws_route53_zone.sirius_opg_digital.id}"
-  records = ["${aws_acm_certificate.opg_api_gateway.domain_validation_options.0.resource_record_value}"]
-  ttl     = 60
-}
-
-resource "aws_acm_certificate_validation" "opg_api_gateway" {
-  certificate_arn         = "${aws_acm_certificate.opg_api_gateway.arn}"
-  validation_record_fqdns = ["${aws_route53_record.opg_api_gateway_certificate_validation.fqdn}"]
 }
 
 output "opg_sirius_api_gateway_custom_url" {
