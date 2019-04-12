4bbdbfb0506b5a2c368b0cce6df1e038ea5fb871
Neil Smith <github@nsmith.me.uk>
2019-04-12 13:58:51 +0100

    Added v1 deployment
    Left old one in for now for testing

--- api-gateway.tf
+++ api-gateway.tf
@@ -83,6 +83,23 @@ resource "aws_api_gateway_deployment" "deployment" {
   }
 }
 
+resource "aws_api_gateway_deployment" "deployment_v1" {
+  rest_api_id = "${aws_api_gateway_rest_api.opg_api_gateway.id}"
+  stage_name  = "v1"
+
+  // The policy is dependent on the module completing, so we can depend on that to mean everything is in place
+  depends_on = ["aws_iam_role_policy_attachment.lpa_online_tool_get_lpas_id_access_policy"]
+
+  variables {
+    // Force a deploy on every apply.
+    deployed_at = "${timestamp()}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
 //------------------------------------
 // Stage level settings
 
