c184a58bdbf412d02948609999197d319d3da737
Neil Smith <github@nsmith.me.uk>
2019-04-17 18:25:20 +0100

    Fixed unittest

--- lambdas/tests/data_providers/test_model.py
+++ lambdas/tests/data_providers/test_model.py
@@ -1,11 +1,18 @@
 import json
+import hashlib
+from datetime import datetime, timezone
 from data_providers import Response
 
 
 class TestProviderResponse(object):
 
     def test_is_empty_when_directly_instantiated(self):
-        r = Response(None)
+        r = Response(
+            ident='abc',
+            payload=None,
+            meta_datetime=datetime.utcnow().replace(tzinfo=timezone.utc).isoformat(),
+            meta_hash=hashlib.sha1('fake-json'.encode()).hexdigest()
+        )
         assert r.is_empty()
 
     def test_is_empty_when_empty_with_none(self):
