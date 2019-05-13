2a1672ce88476a3fea44c0808b2a8e253e0ad74d
Neil Smith <github@nsmith.me.uk>
2019-05-13 17:35:33 +0100

    Updated docker-compose integrations

--- localstack-config/config.sh
+++ localstack-config/config.sh
@@ -3,7 +3,7 @@
 #
 #  Setup DynamoDB
 #
-/usr/local/bin/waitforit -address=tcp://gateway-localstack:4569 -timeout 60 -retry 6000 -debug
+/usr/local/bin/waitforit -address=tcp://gateway-localstack:14569 -timeout 60 -retry 6000 -debug
 
 if [ $? -ne 0 ]; then
     echo "DynamoDB failed to start"
@@ -14,7 +14,7 @@ else
     --key-schema AttributeName=id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10 \
     --region eu-west-1 \
-    --endpoint http://gateway-localstack:4569
+    --endpoint http://gateway-localstack:14569
 
     aws dynamodb create-table \
     --attribute-definitions AttributeName=id,AttributeType=S \
@@ -22,11 +22,11 @@ else
     --key-schema AttributeName=id,KeyType=HASH \
     --provisioned-throughput ReadCapacityUnits=10,WriteCapacityUnits=10 \
     --region eu-west-1 \
-    --endpoint http://gateway-localstack:4569
+    --endpoint http://gateway-localstack:14569
 
     aws dynamodb update-time-to-live \
     --table-name opg-gateway-cache-data \
     --time-to-live-specification "Enabled=true, AttributeName=expires" \
     --region eu-west-1 \
-    --endpoint http://gateway-localstack:4569
+    --endpoint http://gateway-localstack:14569
 fi
