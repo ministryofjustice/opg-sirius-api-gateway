32bbd5b60c8f687dd1c11d3c933e95b4a60ea281
Tom Withers <thomas.withers@digital.justice.gov.uk>
2020-01-20 10:34:09 +0000

    SP-188 Remove API gateway account resource from state
    Co-authored-by: James Warren <james.warren@digital.justice.gov.uk>

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,36 +1,6 @@
 # Defines the gateway, its settings, and the deploy
 
 //------------------------------------
-// Setup account level logging
-// TODO: Move out of product specific code
-
-resource "aws_api_gateway_account" "opg_api_gateway" {
-  cloudwatch_role_arn = "${aws_iam_role.cloudwatch.arn}"
-}
-
-resource "aws_iam_role" "cloudwatch" {
-  name = "api_gateway_cloudwatch_global"
-
-  assume_role_policy = "${data.aws_iam_policy_document.assume_role.json}"
-}
-
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["apigateway.amazonaws.com"]
-    }
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "log_to_cloudwatch" {
-  role       = "${aws_iam_role.cloudwatch.id}"
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"
-}
-
-//------------------------------------
 // Setup the API Gateway
 
 resource "aws_api_gateway_rest_api" "opg_api_gateway" {
